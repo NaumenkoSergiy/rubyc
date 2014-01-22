b3d8a3d37c8b99e9ee39db19cefc61ba9abe2ef5
Alexander Manzyuk <admsev@gmail.com>
2014-01-22 09:37:48 +0200

    remove old foto column from speaker

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140118160159) do
+ActiveRecord::Schema.define(version: 20140121190605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 20140118160159) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "about"
-    t.string   "foto"
     t.boolean  "approved",   default: false, null: false
   end
 
