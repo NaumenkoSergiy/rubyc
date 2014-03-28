13401da33caf297f9fb2a9e6bab5e8b8631fda96
Alexander Manzyuk <admsev@gmail.com>
2014-03-28 11:56:10 +0200

    production env tunes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with :name => "admin", :password => "Crjkzhbz!"
+  http_basic_authenticate_with :name => "admin", :password => "Crjkzhbz!" if Rails.env.production?
 
   def show
     @speaker = Speaker.find(params[:id])
