bbc9910d6d80c88cc185b379167005be7cdd261b
Alexander Manzyuk <admsev@gmail.com>
2014-03-28 16:49:19 +0200

    slim: pretty for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Rubyc2014::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  Slim::Engine.set_default_options pretty: true
 end
