55b6404e4ecae89d59d4a6bd9a2e33205a0763ca
Alexander Manzyuk <admsev@gmail.com>
2014-06-16 14:51:18 +0300

    devised videos added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # http_basic_authenticate_with :name => "admin", :password => "Crjkzhbz!" if Rails.env.production?
+  layout :layout_by_resource
 
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rubyc2014::Application.routes.draw do
-  devise_for :visitors
   devise_for :admins
+  devise_for :visitors
   mount Ckeditor::Engine => '/ckeditor'
   resources :posts
-  
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
